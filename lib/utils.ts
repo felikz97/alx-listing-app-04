fe943e71dc87a18157543dbb533b7ff4d94c7905
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-07-12 16:47:19 +0300

    PROJECT INITIALIZATION

---
+++ lib/utils.ts
@@ -0,0 +1,8 @@
+// lib/utils.ts
+export function formatDate(date: Date): string {
+  return date.toLocaleDateString("en-US");
+}
+
+export function formatTime(date: Date): string {
+  return date.toLocaleTimeString("en-US", { hour: '2-digit', minute: '2-digit' });
+}
